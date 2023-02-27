e897919c9fbf409d7098b0b8ae75d541ad7e1564
Sebastian <Sebastian.veggerby@gmail.com>
2023-02-27 10:01:22 +0100

    Vores start besked tilføjet i programmet

--- H1-Første/Program.cs
+++ H1-Første/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hej, Velkommen til vores lommeregner ! :)");
+
+Console.WriteLine("Indtast Venligst et tal "); 
