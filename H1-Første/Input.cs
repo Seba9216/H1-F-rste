697e241099a98c5795d561d5597fcbae53e1a4c6
Sebastian <Sebastian.veggerby@gmail.com>
2023-02-27 10:46:30 +0100

    Denne funktion tilføjer bruger input samt en måde at minus og dividere

---
+++ H1-Første/Input.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Første
+{
+    public class Input
+    {
+        public static string input()
+        {
+            string Input = Console.ReadLine();
+            return Input;
+        }
+
+        public static bool isValid(string res)
+        {
+            if (res == null)
+            {
+                Console.WriteLine("Skriv et tal din nar"); 
+                return false;
+            }
+
+            try 
+            { 
+                Convert.ToInt64(res); 
+                return true;
+            }
+            
+            catch(Exception e)
+            { Console.WriteLine("Du skal vælge et tal og ikke alt mulig andet skidt"); return false;   }
+        }
+
+    }
+}
---
+++ H1-Første/Plus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Første
+{
+    public class Plus{
+
+        public Plus(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(x + y == result);
+           
+        }
+    }
+}
