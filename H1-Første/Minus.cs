0f56c8be2ba0725f3b382de84922f208d6681013
Sebastian <Sebastian.veggerby@gmail.com>
2023-02-27 10:46:30 +0100

    Denne funktion tilføjer bruger input samt en måde at minus og dividere

---
+++ H1-Første/Minus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Første
+{
+    public class Minus
+    {
+        public static int minus(int i, int j)
+        {
+            Console.WriteLine($" {i} - {j} giver {i - j}"); 
+            return i - j;
+        }
+        public static int divi(int i, int j)
+        {
+            if(i == 0 && j == 0)
+            {
+                Console.WriteLine("Du må ikke dividere med 0 din SO"); 
+                return 0; 
+            }
+            Console.WriteLine($"din divison stykker giver {i / j}"); 
+            return i / j;
+        }
+    }
+}
--- H1-Første/Program.cs
+++ H1-Første/Program.cs
@@ -1,6 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using H1_Første;
+
 Console.WriteLine("Hej, Velkommen til vores lommeregner ! :)");
 
 Console.WriteLine("Indtast Venligst et tal ");
 
-var f = Console.ReadLine();
+string start = Input.input();
+bool valid = Input.isValid(start);
+
+if (valid)
+{
+    Console.WriteLine("Indtast venligst et tal");
+    string start1 = Input.input();
+    bool valid1 = Input.isValid(start1);
+    if (valid1)
+    {
+        int tal1 = Convert.ToInt32(start1);
+        int tal = Convert.ToInt32(start);
+        Console.WriteLine("Vælg venligst en regenmetode :)"); 
+        Minus.minus(tal1, tal);
+    }
+
+
+}
+
+
+
+
+
